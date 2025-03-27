e649c0e86e63410ccc04b075d5e447f6e90c3e4e
Ezra Huska <158761944+Ezra-Huska@users.noreply.github.com>
2025-03-26 21:22:02 -0500

    Update Backtransform_Gcode.py

--- Backtransform_Gcode.py
+++ Backtransform_Gcode.py
@@ -1,30 +1,35 @@
 import re
 import numpy as np
 import time
-import math
 
 # -----------------------------------------------------------------------------------------
 # Transformation Settings
 # -----------------------------------------------------------------------------------------
-FILE_NAME = 'EN_cura_out.gcode'#'EN_cube_out.gcode'      # filename including extension
+FILE_NAME = 'Shape-Cyl.gcode'      # filename including extension
 FOLDER_NAME = 'gcodes/'                              # name of the subfolder in which the gcode is located
-CONE_ANGLE = 16                                      # transformation angle
+CONE_ANGLE = 32                                      # transformation angle
 CONE_TYPE = 'outward'                                # type of the cone: 'inward' & 'outward'
 FIRST_LAYER_HEIGHT = 0.2                            # moves all the gcode up to this height. Use also for stacking
 PLATE_X = 110                                       # moves your gcode away from the origin into the center of the bed (usually bed size / 2)
 PLATE_Y = 110
 X_MOVE = 0
 Y_MOVE = 0
+theta = np.radians(CONE_ANGLE)
 
-def replace_z(x_val,y_val,z_val,e_val):
-    x,y= x_val-PLATE_X,y_val-PLATE_Y
+def move(x,y):
+    x,y = x-220,y-220
+    return x,y
+
+def replace_z(x,y,z_val,e_val):
     if CONE_TYPE == 'outward':
-        z_val = z_val - (math.tan(math.radians(CONE_ANGLE)) * math.sqrt(x**2+y**2))
+        c = -1
     elif CONE_TYPE == 'inward':
-        z_val = z_val + (math.tan(math.radians(CONE_ANGLE)) * math.sqrt(x**2+y**2))
-    
-    row_new = "G1"+" X"+str(round(x_val+X_MOVE,3))+" Y"+str(round(y_val+Y_MOVE,3))+" Z"+str(round(z_val+FIRST_LAYER_HEIGHT,3))+" E"+str(e_val)+"\n"
+        c = 1
+    z_val = z_val + c * (np.tan(theta) * np.sqrt(x**2+y**2))
+    #move(x,y,1)
+    row_new = "G1"+" X"+str(round(x,3))+ " Y"+str(round(y,3))+" Z"+str(round(z_val+FIRST_LAYER_HEIGHT,3))+" E"+str(e_val)+"\n"
     return row_new
+
 
 def backtransform_data(data):
     new_data = []
@@ -36,8 +41,7 @@ def backtransform_data(data):
 
     x_new, y_new = 0, 0
     z_layer = 0
-    e_old = 0
-
+    e_new = 0
     for row in data:
         g_match = re.search(pattern_G, row)
 
@@ -60,9 +64,7 @@ def backtransform_data(data):
                     y_new = float(y_match.group(0).replace('Y', ''))
                 if e_match is not None:
                     e_new = float(e_match.group(0).replace('E', ''))
-                else:
-                    e_new = e_old 
-                
+                x_new,y_new = move(x_new,y_new)
                 new_data.append(replace_z(x_new,y_new,z_layer,e_new))
     return new_data
 
@@ -76,7 +78,7 @@ def main(path):
     data_bt = [row + ' \n' for row in data_bt_string.split('\n')]
     data_bt_string = ''.join(data_bt)
     path_write = re.sub(r'gcodes', 'gcodes_backtransformed', path)
-    path_write = re.sub(r'.gcode','_bt_.gcode', path_write)
+    path_write = re.sub(r'.gcode','_bt.gcode', path_write)
     print(path_write)
     with open(path_write, 'w+') as f_gcode_bt:
         f_gcode_bt.write(data_bt_string)
