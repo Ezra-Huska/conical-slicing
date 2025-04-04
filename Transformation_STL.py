0b527afd1a33c4abfebc0528760c02c0a029560d
Ezra Huska <158761944+Ezra-Huska@users.noreply.github.com>
2025-04-03 19:24:43 -0500

    Update Transformation_STL.py

--- Transformation_STL.py
+++ Transformation_STL.py
@@ -1,30 +1,17 @@
-import numpy as np
 from stl import mesh
+import numpy as np
 import time
-
 
 #-----------------------------------------------------------------------------------------
 # Transformation Settings
 #-----------------------------------------------------------------------------------------
 
-FILE_NAME = 'EN_cube1'                       # Filename without extension
+FILE_NAME = 'EN_cube'                              # Filename without extension
 FOLDER_NAME_UNTRANSFORMED = 'stl/'
 FOLDER_NAME_TRANSFORMED = 'stl_transformed/'    # Make sure this folder exists
 CONE_ANGLE = 16                                 # Transformation angle
-REFINEMENT_ITERATIONS = 5                     # refinement iterations of the stl. 2-3 is a good start for regular stls. If its already uniformaly fine, use 0 or 1. High number cause huge models and long script runtimes
-TRANSFORMATION_TYPE = 'outward'                 # type of the cone: 'inward' & 'outward'
-
-
-def transformation(points, cone_angle_rad, cone_type):
-    if cone_type == 'outward':
-        c = 1
-    elif cone_type == 'inward':
-        c = -1
-    else:
-        raise ValueError('{} is not a admissible type for the transformation'.format(cone_type))
-    transformation_method = (lambda x, y, z: np.array([x/np.cos(cone_angle_rad), y/np.cos(cone_angle_rad), z + c * np.sqrt(x**2 + y**2)*np.tan(cone_angle_rad)]))
-    points_transformed = list(map(transformation_method, points[:, 0], points[:, 1], points[:, 2]))
-    return np.array(points_transformed)
+REFINEMENT_ITERATIONS = 3                       # refinement iterations of the stl. 2-3 is a good start for regular stls. If its already uniformaly fine, use 0 or 1. High number cause huge models and long script runtimes
+CONE_TYPE = 'outward'                           # type of the cone: 'inward' & 'outward'
 
 
 def refinement_four_triangles(triangle):
@@ -42,26 +29,38 @@ def refinement_four_triangles(triangle):
     return np.array([triangle1, triangle2, triangle3, triangle4])
 
 
-def refinement_triangulation(triangle_array, num_iterations):
+def refinement_triangulation(triangle_array):
     #Compute a refinement of a triangulation using the refinement_four_triangles function.
     #The number of iteration defines, how often the triangulation has to be refined; n iterations lead to
     #4^n times many triangles.
     refined_array = triangle_array
-    for i in range(0, num_iterations):
+    for i in range(0, REFINEMENT_ITERATIONS):
         n_triangles = refined_array.shape[0]*4
         refined_array = np.array(list(map(refinement_four_triangles, refined_array)))
         refined_array = np.reshape(refined_array, (n_triangles, 3, 3))
     return refined_array
 
 
-def transformation_STL_file(path, cone_type, cone_angle_deg, nb_iterations):
+def transformation(points):
+    # Transfoms the points on the triagle bassed on their position to the center
+    if CONE_TYPE == 'outward':
+        c = 1
+    elif CONE_TYPE == 'inward':
+        c = -1
+    else:
+        raise ValueError('{} is not a admissible type for the transformation'.format(CONE_TYPE))
+    transformation_method = (lambda x, y, z: np.array([x/np.cos(np.radians(CONE_ANGLE)), y/np.cos(np.radians(CONE_ANGLE)), z + c * np.sqrt(x**2 + y**2)*np.tan(np.radians(CONE_ANGLE))]))
+    points_transformed = list(map(transformation_method, points[:, 0], points[:, 1], points[:, 2]))# makes it work in list form
+    return np.array(points_transformed)
 
-    cone_angle_rad = cone_angle_deg / 180 * np.pi
+
+def main(path):
+    # takes the triangles from stl reifines them, transforms them, then makes a file with the data
     my_mesh = mesh.Mesh.from_file(path)
     vectors = my_mesh.vectors
-    vectors_refined = refinement_triangulation(vectors, nb_iterations)
+    vectors_refined = refinement_triangulation(vectors)
     vectors_refined = np.reshape(vectors_refined, (-1, 3))
-    vectors_transformed = transformation(vectors_refined, cone_angle_rad, cone_type)
+    vectors_transformed = transformation(vectors_refined)
     vectors_transformed = np.reshape(vectors_transformed, (-1, 3, 3))
     my_mesh_transformed = np.zeros(vectors_transformed.shape[0], dtype=mesh.Mesh.dtype)
     my_mesh_transformed['vectors'] = vectors_transformed
@@ -69,7 +68,7 @@ def transformation_STL_file(path, cone_type, cone_angle_deg, nb_iterations):
     return my_mesh_transformed
 
 start = time.time()
-transformed_STL = transformation_STL_file(path=FOLDER_NAME_UNTRANSFORMED + FILE_NAME + '.stl', cone_type=TRANSFORMATION_TYPE, cone_angle_deg=CONE_ANGLE, nb_iterations=REFINEMENT_ITERATIONS)
-transformed_STL.save(FOLDER_NAME_TRANSFORMED + FILE_NAME + 'transformed.stl')
+transformed_STL = main(path=FOLDER_NAME_UNTRANSFORMED + FILE_NAME + '.stl')
+transformed_STL.save(FOLDER_NAME_TRANSFORMED + FILE_NAME + '_tf.stl')
 end = time.time()
 print('Transformation time:', end - start)
